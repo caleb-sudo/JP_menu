075784847a471e5ddb21d9a6fd89670c6da560c2
sucralfateoleophobicity <123672600+caleb-sudo@users.noreply.github.com>
2026-03-12 12:17:02 -0600

    Modify root route to fetch and return DB version
    Updated root route to return database version.

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express();
+require("dotenv").config();
 
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
+
+const sql = neon(process.env.DATABASE_URL);
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-    res.send("Hello world!");
+app.get("/", async (req, res) => {
+    const result = await sql`SELECT version()`;
+    const { version } = result[0];
+    res.send(result);
+    res.end(version);
 });
 
 app.listen(port, () => {
