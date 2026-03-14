fcfa1cdceed3afebe5f6167f2bf655ac00a51c81
sucralfateoleophobicity <123672600+caleb-sudo@users.noreply.github.com>
2026-03-13 20:04:00 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import { neon } from '@neondatabase/serverless';
 
 export default function Page() {
+<<<<<<< HEAD
+  async function create() {
+    'use server';
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment');
+    await sql('CREATE TABLE IF NOT EXISTS comments (comment TEXT);');
+=======
   async function create(formData: FormData) {
     'use server';
     const sql = neon(`${process.env.DATABASE_URL}`);
     const comment = formData.get('comment');
+>>>>>>> c80d396c7c88418e4614b0ad89cc2a08452de1dd
     await sql('INSERT INTO comments (comment) VALUES ($1)', [comment]);
   }
 
@@ -14,4 +22,8 @@ export default function Page() {
       <button type="submit">Submit</button>
     </form>
   );
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> c80d396c7c88418e4614b0ad89cc2a08452de1dd
