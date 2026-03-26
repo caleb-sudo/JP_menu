925ed5394803b35a714c9ae706e8ccc84fc93be5
sucralfateoleophobicity <123672600+caleb-sudo@users.noreply.github.com>
2026-03-26 15:56:16 -0600

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/api/orders/pickup/cafe/count', async (req, res) => {
 app.post('/api/orders/pickup/cafe', async (req, res) => {
 });
 app.delete('/api/orders/pickup/cafe:ordernum', async (req, res) => {
-    /*let { ORDER_NUM } = req.param.ordernum;
+    /*const ORDER_NUM = req.param.ordernum;
     const query = sql`DELETE FROM cafePickup WHERE ordernum = ?`;
     connection.query(query, {error, request, field} => {
         if (error) return console.error("Error: " + error);
